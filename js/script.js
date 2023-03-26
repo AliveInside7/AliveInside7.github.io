d498ec1ad16fc528ff1c70a75089dbac4fe1b17a
AliveInside7 <128882070+AliveInside7@users.noreply.github.com>
2023-03-26 08:20:32 +0500

    new 26.03 slider ready

--- js/script.js
+++ js/script.js
@@ -1,27 +1,37 @@
-
 $(document).ready(function () {
-    $("#comments-slider").owlCarousel({
+  $("#comments-slider").owlCarousel({
     navigation: false,
     slideSpeed: 100,
     paginationSpeed: 800,
     singleItem: true,
-    autoPlay: false
-    });
-    });
+    autoPlay: false,
+  });
+});
 
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        padding: 0.1,
-        nav:false,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:5
-            }
-        }
-    })
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  padding: 0.1,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
+
+$(document).ready(function () {
+    $("#comments-slider2").owlCarousel({
+      navigation: false,
+      width: 300,
+      slideSpeed: 100,
+      paginationSpeed: 800,
+      singleItem: true,
+      autoPlay: false,
+    });
+  });
