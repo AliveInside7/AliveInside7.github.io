76a33051ce8efc327fef109f91a93e33d74af8f4
AliveInside7 <128882070+AliveInside7@users.noreply.github.com>
2023-03-27 10:16:39 +0500

    big

--- js/script.js
+++ js/script.js
@@ -63,3 +63,37 @@ function changeText(identifier2) {
 // function changeText(identifier) {
 //  identifier.style.text-decoration = dec;
 // }
+
+let button = document.getElementById('button');
+let menu = document.getElementById('menu');
+let wrap = document.getElementById('wrap');
+let fixedBlock = document.getElementById('fixed-block');
+
+
+button.addEventListener('click', function() {
+  menu.classList.toggle("menuMoveLeft");
+  wrap.classList.toggle("wrapMoveLeft");
+  fixedBlock.classList.toggle("wrapMoveLeft");
+});
+
+// 
+
+const boxes = Array.from(document.querySelectorAll(".box")); // считываем все элементы аккордеона в массив
+
+boxes.forEach((box) => {
+  box.addEventListener("click", boxHandler); // при нажатии на бокс вызываем ф-ию boxHanlder
+});
+
+function boxHandler(e) {
+  e.preventDefault(); // сбрасываем стандартное поведение
+  let currentBox = e.target.closest(".box"); // определяем текущий бокс
+  let currentContent = e.target.nextElementSibling; // находим скрытый контент
+  currentBox.classList.toggle("active"); // присваиваем ему активный класс
+  if (currentBox.classList.contains("active")) {
+    // если класс активный ..
+    currentContent.style.maxHeight = currentContent.scrollHeight + "px"; // открываем контент
+  } else {
+    // в противном случае
+    currentContent.style.maxHeight = 0; // скрываем контент
+  }
+}
